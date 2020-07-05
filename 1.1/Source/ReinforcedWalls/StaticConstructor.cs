0b663e3ba59e3b58b13837ec08d73de54ab59641
Neceros <neceros@gmail.com>
2020-07-04 22:46:29 -0700

    big update

---
+++ 1.1/Source/ReinforcedWalls/StaticConstructor.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace ReinforcedWalls
+{
+  [StaticConstructorOnStartup]
+  public static class Startup
+  {
+    static Startup()
+    {
+      // Loads right before main menu
+      RWModSettings.ReinforcedWall = ThingDef.Named("NEC_ReinforcedWall");
+      RWModSettings.ReinforcedEmbrasure = ThingDef.Named("NEC_ReinforcedEmbrasure");
+
+      // RWModSettings.OriginalDesignationCat = RWModSettings.ReinforcedEmbrasure.designationCategory;
+
+      RWMod.UpdateChanges();
+    }
+  }
+}
